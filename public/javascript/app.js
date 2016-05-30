62cc826e708157ca968b125590fe28eb88ff4bcc
M-Doja <micdoja@yahoo.com>
2016-05-29 21:08:00 -0700

    initial push. full login/reg auth with tracking

---
+++ public/javascript/app.js
@@ -0,0 +1,12 @@
+// STICKY NAV --------------------------------------------------------
+var win      = $(window),
+    fxel     = $('nav'),
+    eloffset = fxel.offset().top;
+
+win.scroll(function() {
+    if (eloffset < win.scrollTop()) {
+        fxel.addClass("fixed");
+    } else {
+        fxel.removeClass("fixed");
+    }
+});
