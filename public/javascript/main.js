c5bed066c616bbb2da2cc8fd5f29711bf0e51e57
M-Doja <micdoja@yahoo.com>
2016-05-30 17:05:12 -0700

    Bringing back indivual user data on log. Displaying some data on profile page

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,54 +2,143 @@
     "use strict";
     $(".hideDash").css("display", "none");
 
-
-
-
-
     // the main firebase reference
-    var rootRef = new Firebase('https://shakedown.firebaseio.com/web/uauth');
+    var rootRef = new Firebase('https://shakedown.firebaseio.com/');
+    var rootUser = rootRef.child('users');
 
     // Attach an asynchronous callback to read the data at our posts reference
     rootRef.on("value", function(snapshot) {
       document.getElementById('showHere').innerHTML = '';
-      console.log(snapshot.val());
-      var res = snapshot.val();
-      // var Users = [];
-      // var user;
-      // for (var prop in res){
-      //    for(var prop2 in res[prop]) {
-      //      user = res[prop][prop2];
-      //      Users.push(user);
-      //      console.log(Users);
-      //      for (var i = 0; i < Users.length; i++) {
-      //        console.log(Users[i]);
-      //      }
-      //      console.log(user.fName);
-      //      var str = "<h1>" +user.fName + "</h1><br><h4>" + user.groupName + "<br>"
-      //      + user.tagLine + "<br>"
-      //      + "<img src='"+ user.photo+"'> ";
-      //      document.getElementById('showHere').innerHTML += str;
-      //    }
-      //  }
-    }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
+      var data = snapshot.val();
+    },
+      function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
     });
+
+    rootUser.orderByChild("name").equalTo('hog').on("child_added", function(snapshot) {
+      // console.log(snapshot.val().name);
+      var currentUserName = snapshot.val().name;
+      $(".name").html( currentUserName )
+    });
+    rootUser.orderByKey().on("child_added", function(snapshot) {
+      // console.log(snapshot.key());
+      // console.log(snapshot.val().name);
+    });
+    var isNewUser = true;
+    rootRef.onAuth(function(authData) {
+      // console.log(authData);
+      if (authData && isNewUser) {
+        rootRef.child('users').once("value", function(snapshot) {
+          snapshot.forEach(function(childSnapshot) {
+            var key = childSnapshot.key();
+            var childData = childSnapshot.val();
+            // console.log(childData.name);
+            // console.log(authData.uid);
+            if (authData.uid === key) {
+              var currentUserName = childData.name;
+              $(".name").html( currentUserName )
+              console.log('Right now ' + childData.name + ', with user ID:' + authData.uid + ' is logged in');
+            }
+          });
+        });
+      }
+    });
+    rootRef.child('users').once("value", function(snapshot) {
+      // The callback function will get called twice, once for "fred" and once for "barney"
+      snapshot.forEach(function(childSnapshot) {
+        var key = childSnapshot.key();
+        // console.log(key);
+        // childData will be the actual contents of the child
+        var childData = childSnapshot.val();
+        // console.log(childData.name);
+      });
+    });
+    rootUser.once("value", function(snapshot) {
+      var a = snapshot.numChildren();
+      // a === 1 ("name")
+      // console.log(a);
+    });
+
+    var ref = new Firebase("https://dinosaur-facts.firebaseio.com/dinosaurs");
+    ref.orderByChild("height").equalTo(25).on("child_added", function(snapshot) {
+      // console.log(snapshot.key());
+    });
+    ref.orderByKey().on("child_added", function(snapshot) {
+      // console.log(snapshot.key());
+    });
+    ref.orderByChild("height").startAt(3).on("child_added", function(snapshot) {
+    // console.log(snapshot.key())
+  });
+  ref.orderByKey().endAt("linhenykus").on("child_added", function(snapshot) {
+    // console.log(snapshot.key());
+  });
+  var ref1 = new Firebase("https://dinosaur-facts.firebaseio.com/");
+  ref1.orderByChild("height").limitToFirst(3).on("child_added", function(snapshot) {
+    // console.log(snapshot.key());
+  });
+  var ref2 = new Firebase("https://samplechat.firebaseio-demo.com/users");
+  var query = ref2.limitToFirst(5);
+  var refToSameLocation = query.ref()
+    // var scoresRef = new Firebase("https://dinosaur-facts.firebaseio.com/scores");
+    // scoresRef.orderByValue().limitToLast(3).on("value", function(snapshot) {
+    //   snapshot.forEach(function(data) {
+    //     console.log("The " + data.key() + " dinosaur's score is " + data.val());
+    //   });
+    // });
 
     var isNewUser = true;
     rootRef.onAuth(function(authData) {
       if (authData && isNewUser) {
         $(".hideDash").css('display', 'inline')
-        // save the user's profile into the database so we can list users,
-        // use them in Security and Firebase Rules, and show profiles
-        // rootRef.child("users").child(authData.uid).set({
-        //   provider: authData.provider,
-        //   name: getName(authData),
-        //   data: authData
-        // });
+
+        // var name = $("#userName").val();
+        // console.log(name);
+        rootUser.orderByChild("name").equalTo('Shaggy').on("child_added", function(snapshot) {
+          // console.log(snapshot.val().name);
+        });
+
         // the user's data
-        console.log(authData);
-      }
-    });
+        rootRef.getAuth()
+        if (rootRef.getAuth()) {
+            console.log("Authenticated user with uid:", rootRef.getAuth().uid);
+          }
+        function authDataCallback(authData) {
+          if (authData) {
+            // console.log("User " + authData.uid + " is logged in with " + authData.provider);
+
+            if (rootRef.getAuth().uid === authData.uid) {
+
+                // console.log(rootRef.getAuth().uid);
+
+                var currentUser = rootRef.getAuth().uid;
+
+                // console.log(currentUser);
+
+                  rootRef.orderByKey().on("child_added", function(snapshot) {
+                    // console.log(snapshot.key());
+                  });
+                  // console.log('It\'s a match');
+                }
+            } else {
+                // console.log("User is logged out");
+            }
+          }
+        // Register the callback to be fired every time auth state changes
+        rootRef.onAuth(authDataCallback);
+        }
+      });
+
+    // find a suitable name based on the meta info given by each provider
+function getName(authData) {
+  switch(authData.provider) {
+     case 'password':
+       return authData.password.email.replace(/@.*/, '');
+     case 'twitter':
+       return authData.twitter.displayName;
+     case 'facebook':
+       return authData.facebook.displayName;
+  }
+}
 
     // pair our routes to our form elements and controller
     var routeMap = {
@@ -85,11 +174,6 @@
             controller: 'settings',
             authRequired: true // must be logged in to get here
         },
-            '#/add_profile': {
-            form: 'frmAdd_Profile',
-            controller: 'add_profile',
-            authRequired: true // must be logged in to get here
-        },
     };
 
     // create the object to store our controllers
@@ -118,7 +202,6 @@
                 deferred.resolve(user);
             }
         });
-
         return deferred.promise();
     };
 
@@ -131,12 +214,9 @@
             if (err) {
                 deferred.reject(err);
             }
-
             if (user) {
                 deferred.resolve(user);
-
             }
-
         });
           $("#hideDash").css("display", "inline");
         return deferred.promise();
@@ -153,9 +233,7 @@
             } else {
                 deferred.reject(err);
             }
-
         });
-
         return deferred.promise();
     }
 
@@ -166,7 +244,6 @@
             .then(function () {
             return authWithPassword(userObj);
         });
-
     }
 
     // authenticate anonymously
@@ -174,17 +251,13 @@
     function authAnonymously() {
         var deferred = $.Deferred();
         rootRef.authAnonymously(function (err, authData) {
-
             if (authData) {
                 deferred.resolve(authData);
             }
-
             if (err) {
                 deferred.reject(err);
             }
-
         });
-
         return deferred.promise();
     }
 
@@ -193,10 +266,8 @@
     function handleAuthResponse(promise, route) {
         $.when(promise)
             .then(function (authData) {
-
             // route
             routeTo(route);
-
         }, function (err) {
             console.log(err);
             // pop up error
@@ -205,7 +276,6 @@
                 detail: err.message,
                 className: 'alert-danger'
             });
-
         });
     }
 
@@ -214,7 +284,6 @@
         var title = opts.title;
         var detail = opts.detail;
         var className = 'alert ' + opts.className;
-
         alertBox.removeClass().addClass(className);
         alertBox.children('#alert-title').text(title);
         alertBox.children('#alert-detail').text(detail);
@@ -223,6 +292,7 @@
     /// Controllers
     ////////////////////////////////////////
 
+    //======== LOGIN PAGE
     controllers.login = function (form) {
         // Form submission for logging in
         form.on('submit', function (e) {
@@ -249,44 +319,45 @@
         });
     };
 
-    // logout immediately when the controller is invoked
+    //======== LOGOUT PAGE
     controllers.logout = function (form) {
       $(".hideDash").css("display", "none");
         rootRef.unauth();
         document.getElementById('showHere').innerHTML = '';
     };
 
+    //======== REGISTER PAGE
     controllers.register = function (form) {
+
         // Form submission for registering
         form.on('submit', function (e) {
             var userAndPass = $(this).serializeObject();
             var loginPromise = createUserAndLogin(userAndPass);
          e.preventDefault();
-
             handleAuthResponse(loginPromise, 'profile');
             document.getElementById('showHere').innerHTML = '';
         });
     };
 
+    //======== PROFILE PAGE
     controllers.pro = function (form) {
         var user = rootRef.getAuth();
         var userRef;
     };
 
+    //======== DASHBPARD PAGE
     controllers.dash = function (form) {
         var user = rootRef.getAuth();
         var userRef;
     };
 
-    controllers.add_profile = function (form) {
-        var user = rootRef.getAuth();
-        var userRef;
-    };
-
+    //===== ADD / EDIT PROFILE PAGE
     controllers.profile = function (form) {
         // Check the current user
         var user = rootRef.getAuth();
         var userRef;
+
+
         // If no current user send to register page
         if (!user) {
             routeTo('register');
@@ -295,6 +366,17 @@
         // Load user info
         userRef = rootRef.child('users').child(user.uid);
         userRef.once('value', function (snap) {
+
+          // rootRef.onAuth(function(authData) {
+          //   if (authData && isNewUser) {
+          //     // save the user's profile into the database so we can list users,
+          //     // use them in Security and Firebase Rules, and show profiles
+          //     rootRef.child('users').set({
+          //       provider: authData.provider,
+          //       name: getName(authData)
+          //     });
+          //   }
+          // });
             var user = snap.val();
     // RELOADS THE PAGE TO RETRIEVE DATA FROM DB
             if( window.localStorage )
@@ -310,6 +392,7 @@
             if (!user) {
                 return;
             }
+
             // set the fields
             form.find('#userName').val(user.userName);
             form.find('#fName').val(user.Fname);
@@ -351,6 +434,7 @@
 
     };
 
+    //======== SETTING PAGE
     controllers.settings = function (form) {
         var user = rootRef.getAuth();
         var userRef;
@@ -406,13 +490,15 @@
     ///  #/         - Login
     //   #/logout   - Logut
     //   #/register - Register
-    //   #/profile  - Profile
+    //   #/profile  - Add / Edit Profile
+    //   #/pro      - Profile
+    //   #/dash     - Dashboard
+    //   #/settings - Settings
 
     Path.map("#/").to(prepRoute);
     Path.map("#/logout").to(prepRoute);
     Path.map("#/register").to(prepRoute);
     Path.map("#/profile").to(prepRoute);
-    Path.map("#/add_profile").to(prepRoute);
     Path.map("#/pro").to(prepRoute);
     Path.map("#/dash").to(prepRoute);
     Path.map("#/settings").to(prepRoute);
